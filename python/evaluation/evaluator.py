ea44d23e12c5280293aab33a7cb1c28f839fbd33
Remi <rku16@ic.ac.uk>
2019-03-09 15:48:39 +0000

    Complete LSH search - TODO: formulate ouput and formal testing

--- python/evaluation/evaluator.py
+++ python/evaluation/evaluator.py
@@ -1,47 +1,61 @@
 import numpy as np
 from matplotlib import pyplot as plt 
-from LocalitySensitiveHashing import *
+from nearpy import Engine
+from nearpy.hashes import RandomBinaryProjections
 
-def csv_create_hashing(datafile):
-    lsh = LocalitySensitiveHashing(
-            datafile = datafile,
-            dim = 10,
-            r = 50,
-            b = 100,
-            expected_num_of_clusters = 10,
-    )
-    lsh.get_data_from_csv()
-    lsh.initialize_hash_store()
-    lsh.hash_all_data()
-    similarity_groups = lsh.lsh_basic_for_neighborhood_clusters()
-    coalesced_similarity_groups = lsh.merge_similarity_groups_with_coalescence( similarity_groups )
-    merged_similarity_groups = lsh.merge_similarity_groups_with_l2norm_sample_based( coalesced_similarity_groups )
-    lsh.write_clusters_to_file( merged_similarity_groups, "clusters.txt" )
+def categorize(engine, query):
+    # Illnesses:
+    ill = ['Herpes', 'Syphillis', 'HPV', 'HIV']
 
+    # Get nearest points.
+    nearest = engine.neighbours(query)
 
-def generate_data(filepath):
-    # Generate random values.
-    values = []
-    for _ in range(10):
-        values.append([np.random.uniform(10) for _ in range(5)])
+    # Return results.
+    if len(nearest) == 0:
+        print("Sample suggests host is healthy!")
+        return
+    for candidate in nearest:
+        index = int(candidate[-2][-1])
+        value = float(candidate[-1])
+        print("Sample suggests presence of %s with distance %.2f" % (ill[index], value))
 
-    # Write data to selected file.
-    with open(filepath, 'w') as file:
-        for row in values:
-            val = ((str(row))[1:-1]).replace(",", "") + "\n"
-            file.write(val)
+"""
+Sets correspond to regions which represent diseases.
+When testing for a given input, if it falls inside a region, flag as that illness.
+-> otherwise flag as healthy / probably the closest..? 
+"""
+def create_hashing(sets):
+
+    # Dimension of the vector space.
+    dimension = len(sets[0][0]) 
+
+    # Create a random binary hash with 10 bits
+    rbp = RandomBinaryProjections('rbp', 10)
+
+    # Create engine with pipeline configuration
+    engine = Engine(dimension, lshashes=[rbp])
+
+    # Index all our values (set their data to a unique string). 
+    for index,s in enumerate(sets):
+        for v in s:
+            engine.store_vector(v, 'data_%d' % index)
+
+    return engine
 
 def main():
+    # Each set represents data from a particular disease we are modelling.
+    sets = 4
+    # Number of dimension represents the amt of data used in evaluating disease.
+    dimension = 10
 
-    # Load data from data. 
-    # filepath = str(input("Input image path:\n> "))
-    filepath = "datapoints.txt"
-    generate_data(filepath)
-    data = np.asarray(np.loadtxt(filepath))
-    print(data)
+    # Generate data. 
+    data = [[np.random.randn(dimension) for _ in range(100)] for n in range(sets)]
 
     # Run LSH on the data.
-    create_hashing(data)
+    engine = create_hashing(data)
+    categorize(engine, np.random.randn(dimension))
+
+
 
 if __name__ == '__main__':
  main()
