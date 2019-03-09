dd123752ff06487212f2150d6dba6e9907d46049
Guy Leroy <guy.leroy99@gmail.com>
2019-03-09 17:06:39 +0000

    pictures

--- python/processor.py
+++ python/processor.py
@@ -5,18 +5,41 @@ import matplotlib.pyplot as plt
 
 def process_image(filename):
     image = np.array(Image.open(filename))
-    print(image.shape, image.dtype)
-    empty_img = np.zeros_like(image)
+    image = lips_canny(image)
+    plt.imshow(image)
+    plt.savefig(filename + "test2.PNG")
+    #image[mask] = (0, 255, 0, 255)
+    #print(image)
+    return filename
+
+def lips_canny(image):
+    bilateral_filtered_image = cv2.bilateralFilter(image, 5, 175, 175)
+    edge_detected_image = cv2.Canny(bilateral_filtered_image, 75, 200)
+    cv2.imshow('Edge', edge_detected_image)
+    return edge_detected_image
+
+def analyse_lips(image):
     RED, GREEN, BLUE = (2, 1, 0)
     reds = image[:, :, RED]
     greens = image[:, :, GREEN]
     blues = image[:, :, BLUE]
     #mask = (greens < 35) | (reds > greens) | (blues > greens)
     #mask = (greens > 100) & (reds > 100) & (blues > 100)
-    mask = (greens < 150) | (reds < 150) | (blues < 150)
+    average = image.mean(axis=0).mean(axis=0)
+
+    pixels = np.float32(image.reshape(-1, 4))
+
+    n_colors = 3
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 200, .1)
+    flags = cv2.KMEANS_RANDOM_CENTERS
+
+    _, labels, palette = cv2.kmeans(pixels, n_colors, None, criteria, 10, flags)
+    _, counts = np.unique(labels, return_counts=True)
+
+    dominant = palette[np.argmax(counts)]
+    print(palette)
+
+
+    mask = (greens < average[0]) | (reds < average[1]) | (blues < average[2])
     image[mask] = (0, 0, 0, 255)
-    plt.imshow(image)
-    plt.savefig(filename + "test2.PNG")
-    #image[mask] = (0, 255, 0, 255)
-    #print(image)
-    return filename
+    return image
