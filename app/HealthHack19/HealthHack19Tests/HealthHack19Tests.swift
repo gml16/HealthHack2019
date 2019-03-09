1cb86d9cbe8891043a365e535b999b0c045f1ccc
samumartinf <samumartinfrias@gmail.com>
2019-03-09 13:42:35 +0000

    Adding app for image upload

---
+++ app/HealthHack19/HealthHack19Tests/HealthHack19Tests.swift
@@ -0,0 +1,34 @@
+//
+//  HealthHack19Tests.swift
+//  HealthHack19Tests
+//
+//  Created by Samuel Martin on 09/03/2019.
+//  Copyright © 2019 Samuel Martin. All rights reserved.
+//
+
+import XCTest
+@testable import HealthHack19
+
+class HealthHack19Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
