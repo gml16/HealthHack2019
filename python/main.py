dcfe200bf397dea6044a8de3a1d385b74e99aec9
Remi <rku16@ic.ac.uk>
2019-03-09 12:58:50 +0000

    Flask integration

--- python/main.py
+++ python/main.py
@@ -1,5 +1,12 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello world!"
+
 def main():
-    
+    hello()
 
 if __name__ == "__main__":
     main()
