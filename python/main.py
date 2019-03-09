a1ecd1043c79054dc72dea89ea6b190d141e3490
Remi <rku16@ic.ac.uk>
2019-03-09 20:56:12 +0000

    Added # TODO - DETECT OVAL INSTEAD OF APPLYING MASK # TODO - DETECT OVAL INSTEAD OF APPLYING MASK

--- python/main.py
+++ python/main.py
@@ -6,14 +6,16 @@ def main():
     cancer = process_image(gums, "gums")
 
     tongue_ulcer = "../diagnostic_uploads/syphilis2.png"
-    syphilis_1 = process_image(tongue_ulcer, "tongue_ulcer")
+    # TODO - DETECT OVAL INSTEAD OF APPLYING MASK
+    syphilis_1 = process_image(tongue_ulcer, "tongue_ulcer") 
 
     tongue_patch = "../diagnostic_uploads/syphilis3.png"
     syphilis_2 = process_image(tongue_patch, "tongue_patch")
 
     lips = "../diagnostic_uploads/herpes4.png"
-    herpes = process_image(lips, "lips")
-    
+    # TODO - DETECT OVAL INSTEAD OF APPLYING MASK
+    herpes = process_image(lips, "lips") 
+
 
 if __name__ == "__main__":
     main()
--- python/processor.py
+++ python/processor.py
@@ -59,11 +59,11 @@ def analyse(image, _type):
     return image
 
 def get_mask(_type, greens, reds, blues, average):
-    if (_type == "lips"):
+    if (_type == "lips"): # TODO - DETECT OVAL INSTEAD OF APPLYING MASK
         mask = (greens < average[0]) | (reds < average[1]) | (blues < average[2])
     if (_type == "tongue_patch"):
         mask = (greens < 130) | (blues < 130) | (reds < 130)
-    if (_type == "tongue_ulcer"):
+    if (_type == "tongue_ulcer"): # TODO - DETECT OVAL INSTEAD OF APPLYING MASK
         mask = (greens < 130) | (blues < 130) | (reds < 130)
     if (_type == "gums"):
         mask = (greens > 100) | (blues > 100) | (reds > 100)
